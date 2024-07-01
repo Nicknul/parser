2b4118677a2d6e9492de70220637c1a55a35ff84
Nicknul <jea7730@gmail.com>
2024-07-01 11:08:44 +0900

    test_정호연 : 자스로 타입 설정

--- work-1.js
+++ work-1.js
@@ -1,6 +1,15 @@
 const a = (b) => {
-  let result = b + 10;
-  return result;
+  if (typeof b === 'number') {
+    if (Number.isInteger(b) === true) {
+      let result = b + 10;
+      return result;
+    } else {
+      console.log(`${b}는 정수가 아닙니다.`);
+    }
+  } else {
+    console.log(`${b}의 타입은 숫자가 아닙니다.`);
+  }
 };
 
 console.log(a('조우식'));
+console.log('1' + 1);
